7bb235b6448f816355c565344cb1eda48f26f646
Heer Nayakpara <heernayakpara997@gmail.com>
2024-03-31 00:52:19 +0530

    initial commit

---
+++ lib/SignUp_Password_Screens/Password.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+class Password extends StatelessWidget
+{
+  const Password({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(),
+    );
+  }
+}
